988f6f4a50f30aef8de868ae5f99b4eec2c68e10
franciscobournissen <franciscobournissen@gmail.com>
2023-08-20 10:15:12 -0300

    Solución login

--- js/login.js
+++ js/login.js
@@ -1,7 +1,15 @@
-
-  document.getElementById("login-form").addEventListener("submit", function(event) {
+//prettier-ignore
+document.getElementById("login-form").addEventListener("submit", function(event) {
     event.preventDefault();
-    
+
+    var floatingInput = document.getElementById("floatingInput").value;
+    var floatingPassword = document.getElementById("floatingPassword").value;
+
+    if (floatingInput.trim() === "" || floatingPassword.trim() === "") {
+        alert("Por favor, ingresa tu correo y contraseña.");
+        return;
+    }
+
     var token = (Math.random().toString(36).substr(2));
     
     // Almacenar el token en localStorage
@@ -9,7 +17,4 @@
     
     // Redirigir al usuario a la página index.html
     window.location.href = "index.html";
-  });
-
-
-
+});
